2a1f361b87fcf7fd3002fac0601087d90d19918c
sAdamingo <ad.stelmach@gmail.com>
2022-02-05 15:44:30 +0100

    + implemented changes to pass InvoiceTest.java + replaced ArrayList with HashMap

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,27 +3,35 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private  Collection<Product> products;
+    private Map<Product, Integer> products;
 
     public Invoice() {
-        this.products = new ArrayList<>();
+        this.products = new HashMap<Product, Integer>();
     }
 
     public void addProduct(Product product) {
-        this.products.add(product);
+        if (products.containsKey(product)) {
+            products.put(product, products.get(product) + 1);
+        }
+        this.products.put(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
+
+
         if (quantity <= 0) {
             throw new IllegalArgumentException("Quantity must be greater than zero!");
         }
-        for (int i = 0; i < quantity; i++) {
-                this.products.add(product);
+        if (products.containsKey(product)) {
+            products.put(product, products.get(product) + quantity);
         }
+        this.products.put(product, quantity);
     }
 
 
@@ -33,24 +41,20 @@ public class Invoice {
         if (products.isEmpty()) {
             return result;
         }
-        BigDecimal sum = products.stream().map(prod -> prod.getPrice()).reduce(BigDecimal.ZERO, BigDecimal::add);
-        return sum;
+        return products.entrySet().stream().map(entry -> entry.getKey().getPrice().multiply(new BigDecimal(entry.getValue()))).reduce(BigDecimal.ZERO,BigDecimal::add);
     }
 
     public BigDecimal getTax() {
         if (products.isEmpty()) {
             return BigDecimal.ZERO;
         }
-        BigDecimal sum = products.stream().map(prod -> prod.getPrice().multiply(prod.getTaxPercent())).reduce(BigDecimal.ZERO, BigDecimal::add);
-        return sum;
+        return products.entrySet().stream().map(entry -> entry.getKey().getPrice().multiply(entry.getKey().getTaxPercent()).multiply(new BigDecimal(entry.getValue()))).reduce(BigDecimal.ZERO,BigDecimal::add);
     }
 
     public BigDecimal getTotal() {
         if (products.isEmpty()) {
             return BigDecimal.ZERO;
         }
-        BigDecimal result = new BigDecimal("0");
-        BigDecimal sum = products.stream().map(prod -> prod.getPriceWithTax()).reduce(BigDecimal.ZERO, BigDecimal::add);
-        return sum;
+        return products.entrySet().stream().map(entry -> entry.getKey().getPriceWithTax().multiply(new BigDecimal(entry.getValue()))).reduce(BigDecimal.ZERO,BigDecimal::add);
     }
 }
